76d2473d95a45ca5bea626dc355318355455b74f
RIitF <cool.shinka@gmail.com>
2023-11-10 21:46:18 +0300

    + возможность сервера подключать несколько пользователей  + исправлен sigfault при запуске без аргументов
     * починен вывод битов из строки char*
     * многопоточность

--- main.cpp
+++ main.cpp
@@ -1,46 +1,75 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <thread>
 #include <boost/asio.hpp>
 
 using namespace std;
 using namespace boost::asio;
 
+vector<ip::tcp::socket*> sockets;
+
 void read_byte(const char* chr, size_t size)
 {
-    for (int i = 8*size - 1; i >= 0; i--)
+    for (int j = 0; j < size; j++)
     {
-        cout << (((1 << i)&chr[i]) != 0);
+        for (int i = 7; i >= 0; i--)
+        {
+            cout << (((1 << i)&chr[j]) != 0);
+        }
+        cout << " ";
     }
     cout << "\n";
 }
 
-void server_func()
+void server_message_thread(ip::tcp::socket* socket)
 {
-    io_service service;
-    ip::tcp::endpoint endpoint (ip::tcp::v4(), 6666);
-    ip::tcp::acceptor acceptor(service, endpoint);
-    ip::tcp::socket socket(service);
-
-    acceptor.accept(socket);
-
-    while (socket.is_open())
+    while(socket->is_open())
     {
         int dataSize;
 
-        socket.receive(buffer(&dataSize, sizeof(int)));
+        socket->receive(buffer(&dataSize, sizeof(int)));
         cout << "Recive: " << dataSize << " data size." << "\n";
 
         string message;
         char data [dataSize];
 
-        socket.receive(buffer(data, dataSize));
-        cout << "Recived: " << data << " with size: " << dataSize << "\n";
+        socket->receive(buffer(data, dataSize));
 
         message = string(data, dataSize);
-
-        cout << message << "\n";
+        cout << "Recived: " << message << " with size: " << dataSize << "\n";
 
         read_byte(message.data(), message.size());
+    }
+}
+
+void server_acceptor_thread(io_service* service, bool* running)
+{
+    vector<thread*> messageThreads;
+
+    while(running)
+    {
+        ip::tcp::endpoint endpoint(ip::tcp::v4(), 6666);
+        ip::tcp::acceptor acceptor(*service, endpoint);
+        sockets.push_back(new ip::tcp::socket(*service));
+
+        acceptor.accept(*sockets[sockets.size() - 1]);
+
+        messageThreads.push_back(new thread(bind(server_message_thread, sockets[sockets.size()-1])));
+    }
+}
+
+void server_func()
+{
+    bool running = true;
+    io_service service;
+
+    thread connectionThread(bind(server_acceptor_thread, &service, &running));
+
+    while (running)
+    {
+        string input;
+        getline(cin, input);
     }
 }
 
@@ -77,20 +106,18 @@ void client_func()
 
 int main(int argc, char **argv) {
 
-    string input = argv[1];
+    string input;
 
-    try
-    {
-        if(input == "server")
-            server_func();
-        else if (input == "client")
-            client_func();
-        else
-            cerr << "Неизвестный аргумент.\n";
-    } catch (exception e)
-    {
-        cerr << e.what() << "\n";
-    }
+    if(argc > 1)
+        input = argv[1];
+
+    if(input == "server")
+        server_func();
+    else if (input == "client")
+        client_func();
+    else
+        cerr << "Неизвестный аргумент.\n";
+
 
     return 0;
 }
